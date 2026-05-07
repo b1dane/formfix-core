fc57ecca56df1d859582acb64e6f5ed81bb7e61d
b1dane <byron.dane@gmail.com>
2026-05-07 03:56:03 -0500

    Fix: Rewrite PosePainter with clean implementation

--- lib/widgets/pose_painter.dart
+++ lib/widgets/pose_painter.dart
@@ -5,7 +5,6 @@ class PosePainter extends CustomPainter {
   final Pose pose;
   final Size imageSize;
   final bool isFrontCamera;
-
   PosePainter({required this.pose, required this.imageSize, this.isFrontCamera = true});
 
   @override
@@ -14,66 +13,21 @@ class PosePainter extends CustomPainter {
     final bp = Paint()..color = const Color(0xFF00F5C4).withOpacity(0.6)..strokeWidth = 3..style = PaintingStyle.stroke..strokeCap = StrokeCap.round;
     final wp = Paint()..color = const Color(0xFF7B61FF).withOpacity(0.4)..strokeWidth = 2..style = PaintingStyle.stroke..strokeCap = StrokeCap.round;
     final lm = pose.landmarks;
-
     Offset tr(PoseLandmark k) {
       final x = isFrontCamera ? size.width - k.x * size.width / imageSize.width : k.x * size.width / imageSize.width;
       return Offset(x, k.y * size.height / imageSize.height);
     }
-
     void bn(PoseLandmarkType a, PoseLandmarkType b, {bool w = false}) {
       final la = lm[a]; final lb = lm[b];
       if (la == null || lb == null) return;
       if (la.likelihood < 0.5 || lb.likelihood < 0.5) return;
       canvas.drawLine(tr(la), tr(lb), w ? wp : bp);
     }
-
     void jt(PoseLandmarkType t) {
       final l = lm[t];
       if (l == null || l.likelihood < 0.5) return;
       canvas.drawCircle(tr(l), 5, jp);
     }
-
-    // Draw the pose skeleton and joints
-    // Left side
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow);
-    bn(PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist);
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip);
-    bn(PoseLandmarkType.leftHip, PoseLandmarkType.leftKnee);
-    bn(PoseLandmarkType.leftKnee, PoseLandmarkType.leftAnkle);
-    // Right side
-    bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow);
-    bn(PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist);
-    bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip);
-    bn(PoseLandmarkType.rightHip, PoseLandmarkType.rightKnee);
-    bn(PoseLandmarkType.rightKnee, PoseLandmarkType.rightAnkle);
-    // Center
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder);
-    bn(PoseLandmarkType.leftHip, PoseLandmarkType.rightHip);
-
-    // Draw joints
-    jt(PoseLandmarkType.nose);
-    jt(PoseLandmarkType.leftEye);
-    jt(PoseLandmarkType.rightEye);
-    jt(PoseLandmarkType.leftEar);
-    jt(PoseLandmarkType.rightEar);
-    jt(PoseLandmarkType.leftShoulder);
-    jt(PoseLandmarkType.rightShoulder);
-    jt(PoseLandmarkType.leftElbow);
-    jt(PoseLandmarkType.rightElbow);
-    jt(PoseLandmarkType.leftWrist);
-    jt(PoseLandmarkType.rightWrist);
-    jt(PoseLandmarkType.leftHip);
-    jt(PoseLandmarkType.rightHip);
-    jt(PoseLandmarkType.leftKnee);
-    jt(PoseLandmarkType.rightKnee);
-    jt(PoseLandmarkType.leftAnkle);
-    jt(PoseLandmarkType.rightAnkle);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-}
-
     bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder);
     bn(PoseLandmarkType.leftHip, PoseLandmarkType.rightHip);
     bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip);
@@ -88,32 +42,8 @@ class PosePainter extends CustomPainter {
     bn(PoseLandmarkType.rightKnee, PoseLandmarkType.rightAnkle);
     bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftEar, w: true);
     bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightEar, w: true);
-    for (final t in [PoseLandmarkType.nose, PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder, PoseLandmarkType.leftElbow, PoseLandmarkType.rightElbow, PoseLandmarkType.leftWrist, PoseLandmarkType.rightWrist, PoseLandmarkType.leftHip, PoseLandmarkType.rightHip, PoseLandmarkType.leftKnee, PoseLandmarkType.rightKnee, PoseLandmarkType.leftAnkle, PoseLandmarkType.rightAnkle]) {
-      jt(t);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-}
-
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder);
-    bn(PoseLandmarkType.leftHip, PoseLandmarkType.rightHip);
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip);
-    bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip);
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow);
-    bn(PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist);
-    bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow);
-    bn(PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist);
-    bn(PoseLandmarkType.leftHip, PoseLandmarkType.leftKnee);
-    bn(PoseLandmarkType.leftKnee, PoseLandmarkType.leftAnkle);
-    bn(PoseLandmarkType.rightHip, PoseLandmarkType.rightKnee);
-    bn(PoseLandmarkType.rightKnee, PoseLandmarkType.rightAnkle);
-    bn(PoseLandmarkType.leftShoulder, PoseLandmarkType.leftEar, w: true);
-    bn(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightEar, w: true);
-    for (final t in [PoseLandmarkType.nose, PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder, PoseLandmarkType.leftElbow, PoseLandmarkType.rightElbow, PoseLandmarkType.leftWrist, PoseLandmarkType.rightWrist, PoseLandmarkType.leftHip, PoseLandmarkType.rightHip, PoseLandmarkType.leftKnee, PoseLandmarkType.rightKnee, PoseLandmarkType.leftAnkle, PoseLandmarkType.rightAnkle]) {
-      jt(t);
-    }
+    final types = [PoseLandmarkType.nose, PoseLandmarkType.leftShoulder, PoseLandmarkType.rightShoulder, PoseLandmarkType.leftElbow, PoseLandmarkType.rightElbow, PoseLandmarkType.leftWrist, PoseLandmarkType.rightWrist, PoseLandmarkType.leftHip, PoseLandmarkType.rightHip, PoseLandmarkType.leftKnee, PoseLandmarkType.rightKnee, PoseLandmarkType.leftAnkle, PoseLandmarkType.rightAnkle];
+    for (final t in types) { jt(t); }
   }
 
   @override
